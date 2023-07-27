2447a6f1b6f45b345ea96368f14c337c8fcf2b5b
Tan Le <kyletan24003@gmail.com>
2023-07-27 17:29:42 +1000

    Set back to localhost

--- web_app.py
+++ web_app.py
@@ -4,4 +4,4 @@ from routes import *
 if __name__ == '__main__':
     # Note, you're going to have to change the PORT number
     Timer(1, open_browser).start()
-    socketio.run(app,host='0.0.0.0', port=443,debug=1,use_reloader=True,ssl_context=('local.crt','local.key'))
+    socketio.run(app,host='localhost', port=443,debug=1,use_reloader=True,ssl_context=('local.crt','local.key'))
