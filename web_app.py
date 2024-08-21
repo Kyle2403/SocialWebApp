efc31d7c37f231de3cf3b8c45562a8c5daf268b7
Kyle2403 <139950362+Kyle2403@users.noreply.github.com>
2024-08-21 17:47:07 +1000

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -3,5 +3,4 @@ from routes import *
 # Starting the python applicaiton
 if __name__ == '__main__':
     # Note, you're going to have to change the PORT number
-    Timer(1, open_browser).start()
-    socketio.run(app,host='localhost', port=443,debug=1,use_reloader=False)#,ssl_context=('local.crt','local.key'))
+    socketio.run(app)#,ssl_context=('local.crt','local.key'))
