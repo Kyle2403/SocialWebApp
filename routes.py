0ce237c58ab213ce7311644893d82fd7c37ef6ef
Kyle2403 <139950362+Kyle2403@users.noreply.github.com>
2024-08-24 17:36:07 +1000

    Update routes.py

--- routes.py
+++ routes.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 app.secret_key = "super_duper_secret_key"
 #app.config['SECRET_KEY'] = "dujfhuisdhfashdfiuahsdfuiahsduifhauisdhfiushdfuihsdiufhsiudhfushdfuihsudhfuishfiudshuifh"
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "null"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
